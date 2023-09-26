6f4364df36ed28c39e8b4aabc97ce6783dc9be96
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2023-09-26 00:32:56 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
+import { FaClock } from "react-icons/fa";
+
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+//import { ContadorSimple } from "./contador";
+
 
 //create your first component
 const Home = () => {
+	
+		const [contador, intervalo] = useState(0);
+
+		const Seis = Math.floor((contador / 100000) % 10);
+		const Cinco = Math.floor((contador / 10000) % 10);
+		const Cuatro = Math.floor((contador / 1000) % 10);
+		const Tres = Math.floor((contador / 100) % 10);
+		const Dos = Math.floor((contador / 10) % 10);
+		const Uno = Math.floor(contador % 10);
+	
+
+	
+	setTimeout(() => {
+		intervalo(contador + 1);
+		// console.log(contador)
+	}, 1000);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="Reloj">
+				<FaClock style={{ color: "#99d85a" }} />
+			</div>
+			<div className="seis">{Seis}</div>
+			<div className="cinco">{Cinco}</div>
+			<div className="cuatro">{Cuatro}</div>
+			<div className="tres">{Tres}</div>
+			<div className="dos">{Dos}</div>
+			<div className="uno">{Uno}</div>
 		</div>
 	);
+
 };
 
 export default Home;
